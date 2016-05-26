41c791005248908590193ce4c689ec60a91e3270
yy_yank <yy.yank.dev.blog@gmail.com>
2016-05-26 23:44:53 +0900

    first commit.

---
+++ streak.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+    var isKusa = function (activity, current) {
+        if(current === 0) {
+            return;
+        }
+        var prev = activity[current - 1];
+        var next = activity[current];
+        return prev["kusa"] === "green" && next["kusa"] === "green";
+    }
+    var streak = new Array();
+    var activity = new Array();
+    $("rect").each(function(){
+        var obj = new Object();
+        var date = $(this).attr("data-date");
+        var color = $(this).attr("fill");
+        var kusa = color !== "#eeeeee" ? "green" : "";
+        obj["date"] = date;
+        obj["kusa"] = kusa;
+        activity.push(obj);
+    });
+    var count = 0;
+    for(var i = 0; i < activity.length; i++) {
+        if(isKusa(activity, i)) {
+            count++;
+        } else {
+            streak.push(count);
+            count = 0;
+        }
+    }
+    streak.push(count);
+    alert("[Current Streak]" + count + "\n[Longest Streak]"+ Math.max.apply(null, streak));
+});
